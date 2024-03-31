6ce8f5b946711649cb57f39f73b7079f460311ee
felic <felicia_jow@yahoo.com>
2024-03-31 13:22:25 +0800

    gamestates and teleportation

--- pacman.c
+++ pacman.c
@@ -10,22 +10,36 @@ typedef struct {
     int level;
 } scoreboard;
 
-scoreboard* create_scoreboard(void) {
-    scoreboard* sb = malloc(sizeof(scoreboard)); // Allocate memory
-    if (sb != NULL) {
-        sb->score = -1;
-        sb->level = 1;
-    }
-    return sb;
-}
-
 typedef struct{
     int pos_x;
     int pos_y;
 } player;
 
+typedef enum {
+    Menu,
+    Playing,
+    Paused,
+    End
+} GameState;
+
+typedef struct {
+    GameState currentGameState;
+    player *player;
+    scoreboard *scoreboard;
+    int map[10][10];
+} GameStateFsm;
+
+scoreboard* create_scoreboard(void) {
+    scoreboard* sb = malloc(sizeof(scoreboard)); 
+    if (sb != NULL) {
+        sb->score = 0;
+        sb->level = 1;
+    }
+    return sb;
+}
+
 player* create_player(int x, int y){
-    player *player = malloc(sizeof(player)); // Allocate memory
+    player *player = malloc(sizeof(player)); 
     if (player != NULL) {
         player->pos_x = x;
         player->pos_y = y;
@@ -36,23 +50,33 @@ player* create_player(int x, int y){
 void set_point_map(int map[10][10]) {
     int i, j;
     int num_big_points = 5;
-    srand(time(NULL)); // Initialize random seed
-
     for (i = 0; i < 10; i++) {
-        for (j = 0; j < 10; j++) {
-            if (map[i][j] == 0) {
-                map[i][j] = 2;
-            }
+        map[i][0] = 1; 
+        map[i][9] = 1; 
+        map[0][i] = 1; 
+        map[9][i] = 1; 
+    }
+
+    for (i = 1; i < 9; i++) {
+        for (j = 1; j < 9; j++) {
+            map[i][j] = 2;
         }
     }
+
+    srand(time(NULL)); 
     while (num_big_points > 0) {
-        int x = rand() % 10;
-        int y = rand() % 10;
-        if (map[x][y] == 2) {
+        int x = rand() % 8 + 1; 
+        int y = rand() % 8 + 1;
+        if (map[x][y] == 2) {  
             map[x][y] = 3;
             num_big_points--;
         }
     }
+}
+
+void set_teleportation_points(int map[10][10]) {
+    map[2][2] = 4; 
+    map[7][7] = 4; 
 }
 
 void print_map(int map[10][10], player *player) {
@@ -60,15 +84,17 @@ void print_map(int map[10][10], player *player) {
     for (i = 0; i < 10; i++) {
         for (j = 0; j < 10; j++) {
             if (i == player->pos_x && j == player->pos_y) {
-                printf("C "); // Player's position
+                printf("C ");
             } else if (map[i][j] == 1) {
-                printf("# "); // Wall
+                printf("# "); 
             } else if (map[i][j] == 2) {
-                printf(". "); // Small Point
+                printf(". "); 
             } else if (map[i][j] == 3) {
-                printf("o "); // Big Point
-            } else {
-                printf("  "); // Empty space
+                printf("o "); 
+            } else if (map[i][j] == 4) {
+                printf("T "); 
+            }else {
+                printf("  "); 
             }
         }
         printf("\n");
@@ -78,24 +104,6 @@ void print_map(int map[10][10], player *player) {
 void print_score_board(scoreboard *scoreboard) {
     printf("LEVEL: %d\n", scoreboard->level);
     printf("SCORE: %d\n", scoreboard->score);
-}
-
-void set_noncanonical_mode() {
-    struct termios term;
-    tcgetattr(STDIN_FILENO, &term);
-    term.c_lflag &= ~(ICANON | ECHO); // Disable canonical mode and echo
-    term.c_cc[VMIN] = 0; // Set minimum number of characters to read
-    term.c_cc[VTIME] = 1; // Set timeout for read (0 seconds)
-    tcsetattr(STDIN_FILENO, TCSANOW, &term);
-}
-
-
-// Function to reset terminal attributes to canonical mode
-void set_canonical_mode() {
-    struct termios term;
-    tcgetattr(STDIN_FILENO, &term);
-    term.c_lflag |= (ICANON | ECHO); // Enable canonical mode and echo
-    tcsetattr(STDIN_FILENO, TCSANOW, &term);
 }
 
 void move_player(player *player, int map[10][10], char direction, scoreboard *scoreboard) {
@@ -117,73 +125,153 @@ void move_player(player *player, int map[10][10], char direction, scoreboard *sc
             break;
     }
 
-    // Check if the next position is within bounds and not a wall
+    
     if (next_x >= 0 && next_x < 10 && next_y >= 0 && next_y < 10 && map[next_x][next_y] != 1) {
-        if (map[next_x][next_y] != 1) { // Not a wall
-        // Check if the next position is a dot
+        if (map[next_x][next_y] != 1) { 
             if (map[next_x][next_y] == 2) {
-                map[next_x][next_y] = 0; // "Eat" the dot by setting the position to empty
+                map[next_x][next_y] = 0; 
                 scoreboard->score++;
             }
             else if (map[next_x][next_y] == 3) {
-                map[next_x][next_y] = 0; // "Eat" the dot by setting the position to empty
+                map[next_x][next_y] = 0; 
                 scoreboard->score++;
                 scoreboard->score++;
             }
+            else if (map[next_x][next_y] == 4) {
+            if (next_x == 2 && next_y == 2) {
+                next_x = 7; next_y = 7; 
+            } else if (next_x == 7 && next_y == 7) {
+                next_x = 2; next_y = 2; 
+            }
         }
-            
-        // Update the player's position
+        }
+        
         player->pos_x = next_x;
         player->pos_y = next_y;
     }
+
+}
+
+int all_points_eaten(int map[10][10]) {
+    int i,j;
+    for (i = 0; i < 10; i++) {
+        for (j = 0; j < 10; j++) {
+            if (map[i][j] == 2 || map[i][j] == 3) { 
+                return 0; 
+            }
+        }
+    }
+    return 1; 
+ }
+
+void handleMenuState(GameStateFsm *fsm, char input) {
+    if (input == '\n') {
+        fsm->currentGameState = Playing;
+        system("clear");
+    }
+}
+
+void handlePlayingState(GameStateFsm *fsm, char input) {
+    if (input == 'q') {
+        fsm->currentGameState = End;
+    } else if (input == ' ') {
+        fsm->currentGameState = Paused;
+        printf("Game Paused. Press Spacebar to resume...\n");
+    } else {
+        move_player(fsm->player, fsm->map, input, fsm->scoreboard);
+        if (all_points_eaten(fsm->map)) {
+            fsm->currentGameState = End;
+            printf("\nGame Over!\nCongratulations, you've eaten all the points!\n");
+        }
+    }
+}
+
+
+void handlePausedState(GameStateFsm *fsm, char input) {
+    if (input == ' ') {
+        fsm->currentGameState = Playing;
+        system("clear");
+    }
+}
+
+void initGameStateFsm(GameStateFsm *fsm) {
+    fsm->currentGameState = Menu;
+    set_point_map(fsm->map);
+    fsm->player = create_player(1, 1);
+    fsm->scoreboard = create_scoreboard();
+    set_teleportation_points(fsm->map);
+}
+
+void processGameState(GameStateFsm *fsm, char input) {
+    switch (fsm->currentGameState) {
+        case Menu:
+            handleMenuState(fsm, input);
+            break;
+        case Playing:
+            handlePlayingState(fsm, input);
+            break;
+        case Paused:
+            handlePausedState(fsm, input);
+            break;
+        case End:
+            break;
+    }
+}
+
+void sleep_microseconds(long microseconds) {
+    struct timespec ts;
+    ts.tv_sec = microseconds / 1000000;
+    ts.tv_nsec = (microseconds % 1000000) * 1000;
+    nanosleep(&ts, NULL);
+}
+
+void set_noncanonical_mode() {
+    struct termios term;
+    tcgetattr(STDIN_FILENO, &term);
+    term.c_lflag &= ~(ICANON | ECHO);
+    term.c_cc[VMIN] = 0; 
+    term.c_cc[VTIME] = 1; 
+    tcsetattr(STDIN_FILENO, TCSANOW, &term);
+}
+
+void set_canonical_mode() {
+    struct termios term;
+    tcgetattr(STDIN_FILENO, &term);
+    term.c_lflag |= (ICANON | ECHO); 
+    tcsetattr(STDIN_FILENO, TCSANOW, &term);
 }
 
 int main() {
-    int map[10][10] = {
-    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-    {1, 0, 0, 0, 1, 0, 0, 0, 0, 1},
-    {1, 0, 1, 0, 1, 0, 1, 1, 0, 1},
-    {1, 0, 1, 0, 0, 0, 0, 1, 0, 1},
-    {1, 0, 1, 1, 1, 1, 0, 1, 0, 1},
-    {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-    {1, 0, 1, 1, 0, 1, 1, 1, 0, 1},
-    {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-    {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
-       };
-    set_point_map(map);
-    player *player = create_player(1, 1);
-    scoreboard *scoreboard = create_scoreboard();
-    set_noncanonical_mode(); // Set non-canonical mode
+    GameStateFsm fsm;
+    char input;
+    initGameStateFsm(&fsm);
 
+    set_noncanonical_mode();
     system("clear");
     printf("Welcome to C-man\n");
     printf("Play with ONLY W, A, S, D\n");
-    printf("Press Spacebar to start the game...\n");
+    printf("Press Enter to start the game...\n");
+    printf("Press Spacerbar to pause and resume the game...\n");
     printf("Press q to quit the game...\n");
-    char c, lastDirection = '\0';
 
-    while (1) {
-        if (read(STDIN_FILENO, &c, 1) > 0) {
-            if (c == 'q') { // to quit the game
-                break;
-            }
-            if (c == 'w' || c == 'a' || c == 's' || c == 'd') {
-                lastDirection = c;
-            }
+    while (fsm.currentGameState != End) {
+        if (read(STDIN_FILENO, &input, 1) > 0) {
+            processGameState(&fsm, input);
         }
-        move_player(player, map, lastDirection, scoreboard); // Move player in the last known direction
-        system("clear");
-        print_score_board(scoreboard);
-        print_map(map, player);
-     
-       
-        usleep(10000); // Movement speed
+
+        if (fsm.currentGameState == Playing) {
+            system("clear");
+            print_score_board(fsm.scoreboard);
+            print_map(fsm.map, fsm.player);
+        }
+
+       sleep_microseconds(100000);
     }
 
     set_canonical_mode();
-    free(player); // Free allocated memory
-    free(scoreboard);
+    free(fsm.player);
+    free(fsm.scoreboard);
+
     return 0;
 }
 
